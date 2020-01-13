4051cd048822c2b67c01818ae8a411b2c7585a5c
Kappeh <kieranleep@hotmail.com>
2020-01-13 13:37:06 +0000

    Added FilesAt function to utils

--- utils.go
+++ utils.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 // SliceIndex finds the indx of the first element of a slice that
@@ -23,4 +27,36 @@ func FormatTime(t time.Time) string {
 		t.Hour(), t.Minute(), t.Second(),
 		t.Day(), t.Month(), t.Year(),
 	)
+}
+
+// FilesAt gets a list of all of the files that are within
+// a given directory specified by the RELATIVE path
+// This does include all files within sub-directories
+func FilesAt(path string) ([]string, error) {
+	// Get the absolute path
+	abs, err := filepath.Abs(path)
+	if err != nil {
+		return []string{}, err
+	}
+	result := []string{}
+	err = filepath.Walk(abs, func(path string, info os.FileInfo, err error) error {
+		// This ommits directories
+		if info.IsDir() {
+			return nil
+		}
+		// Get the relative path
+		rel, err := filepath.Rel(abs, path)
+		if err != nil {
+			return err
+		}
+		// Add it to the results
+		result = append(result, rel)
+		return nil
+	})
+	// Handle any errors
+	if err != nil {
+		return []string{}, errors.Wrap(err, "couldn't walk through files")
+	}
+	// Return the result
+	return result, nil
 }
